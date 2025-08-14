c7ccde46de0dbac61db3713d0bbe7f0b0fd5a4bf
Bowen Li <warr3nlee1997@gmail.com>
2025-08-14 14:31:01 -0700

    before entering Google with firefox section

--- play_ground.py
+++ play_ground.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+#Fetch and parse the page
+
 response = requests.get('https://www.geeksforgeeks.org/python/python-programming-language-tutorial/')
 
 #get
@@ -9,4 +11,12 @@ response = requests.get('https://www.geeksforgeeks.org/python/python-programming
 
 #bs4
 soup = BeautifulSoup(response.content, 'html.parser')
-print(soup.prettify())
+#print(soup.prettify())
+
+# Find the main content container
+content_div = soup.find('div', class_='article--viewer_content')
+if content_div:
+    for para in content_div.find_all('p'):
+        print(para.text.strip())
+else:
+    print("No article content found.")
